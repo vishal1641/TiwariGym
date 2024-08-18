3b05a19eb8ea91137a82c72e2cf72493968678ee
vishal1641 <riturajsoren23@gmai.com>
2024-08-18 21:32:20 +0530

    Changed loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,11 +1,39 @@
-import React from 'react';
-import { Stack } from '@mui/material';
-import { InfinitySpin } from 'react-loader-spinner';
+// import React from "react";
+// import { Stack } from "@mui/material";
+// import { InfinitySpin } from "react-loader-spinner";
+
+// const Loader = () => (
+//   <Stack
+//     direction="row"
+//     justifyContent="center"
+//     alignItems="center"
+//     width="100%"
+//   >
+//     <InfinitySpin color="grey" />
+//   </Stack>
+// );
+
+// export default Loader;
+
+import { useState, CSSProperties } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
+
+// const override: CSSProperties = {
+//   display: "block",
+//   margin: "0 auto",
+//   borderColor: "red",
+// };
+
+// let [color, setColor] = useState("#FF0000");
 
 const Loader = () => (
-  <Stack direction="row" justifyContent="center" alignItems="center" width="100%">
-    <InfinitySpin color="grey" />
-  </Stack>
+  <ClipLoader
+    // color={color}
+    size={150}
+    // cssOverride={override}
+    aria-label="Loading Spinner"
+    data-testid="loader"
+  />
 );
 
 export default Loader;
